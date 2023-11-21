edbd299a9e90c8a8739f31d9be84b87288f4ed5b
Cesar Noriega <c79.noriega@gmail.com>
2023-11-21 00:34:28 -0600

    First commit: Project structure.

---
+++ src/main/java/com/example/cnspring6restmvc/CnSpring6RestMvcApplication.java
@@ -0,0 +1,13 @@
+package com.example.cnspring6restmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CnSpring6RestMvcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CnSpring6RestMvcApplication.class, args);
+    }
+
+}
