6708844352d8766a7ee1f9c3b5f213ecf4bc480e
Cesar Noriega <c79.noriega@gmail.com>
2023-11-22 00:24:00 -0600

    Modified Service Layer.

--- src/main/java/com/example/cnspring6restmvc/services/BeerService.java
+++ src/main/java/com/example/cnspring6restmvc/services/BeerService.java
@@ -2,8 +2,10 @@ package com.example.cnspring6restmvc.services;
 
 import com.example.cnspring6restmvc.model.Beer;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface BeerService {
     Beer getBeerById(UUID id);
+    List<Beer> listBeers();
 }
