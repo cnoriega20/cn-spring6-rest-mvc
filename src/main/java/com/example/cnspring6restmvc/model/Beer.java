79838d304b2798262c55d1dcbc4dfd8c84a5c1ef
Cesar Noriega <c79.noriega@gmail.com>
2023-11-21 22:56:21 -0600

    Added:model pojos with lombok.

---
+++ src/main/java/com/example/cnspring6restmvc/model/Beer.java
@@ -0,0 +1,25 @@
+package com.example.cnspring6restmvc.model;
+
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Getter
+@Setter
+@ToString
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Beer {
+    private UUID id;
+    private Integer version;
+    private String beerName;
+    private BeerStyle beerStyle;
+    private String upc;
+    private Integer quantityOnHand;
+    private BigDecimal price;
+    private LocalDateTime createdDate;
+    private LocalDateTime updateDate;
+}
