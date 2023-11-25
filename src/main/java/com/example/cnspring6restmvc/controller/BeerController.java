7807ea08a8aeb5149830815058f955af14070e2f
Cesar Noriega <c79.noriega@gmail.com>
2023-11-25 00:45:40 -0600

    Added Path Variables.

--- src/main/java/com/example/cnspring6restmvc/controller/BeerController.java
+++ src/main/java/com/example/cnspring6restmvc/controller/BeerController.java
@@ -5,7 +5,9 @@ import com.example.cnspring6restmvc.services.BeerService;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -14,6 +16,7 @@ import java.util.UUID;
 @Slf4j
 @AllArgsConstructor
 @RestController
+@RequestMapping("/api/v1/beer")
 public class BeerController {
     private final BeerService beerService;
 
@@ -21,7 +24,8 @@ public class BeerController {
     public List<Beer> listBeers(){
         return beerService.listBeers();
     }
-    public Beer getBeerById(UUID id){
+    @RequestMapping(value = "{beerId}", method = RequestMethod.GET)
+    public Beer getBeerById(@PathVariable("beerId") UUID id){
         log.info("Get Beer by Id - In Controller");
         return beerService.getBeerById(id);
     }
