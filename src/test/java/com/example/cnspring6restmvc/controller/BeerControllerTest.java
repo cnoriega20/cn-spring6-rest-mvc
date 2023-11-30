e9006bf61ef3499bce3ab766194dd0102ed3dd14
Cesar Noriega <c79.noriega@gmail.com>
2023-11-30 00:21:44 -0600

    Added mockMvc test for Beer Controller.

--- src/test/java/com/example/cnspring6restmvc/controller/BeerControllerTest.java
+++ src/test/java/com/example/cnspring6restmvc/controller/BeerControllerTest.java
@@ -1,21 +1,36 @@
 package com.example.cnspring6restmvc.controller;
 
+import com.example.cnspring6restmvc.services.BeerService;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 
 import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.*;
-@SpringBootTest
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(BeerController.class)
 @Slf4j
 class BeerControllerTest {
 
     @Autowired
-    BeerController beerControllerTest;
+    MockMvc mockMvc;
+
+    @MockBean
+    BeerService beerService;
+
     @Test
-    void getBeerById() {
-        log.info( "Get Beer by Id : {} ", beerControllerTest.getBeerById(UUID.randomUUID()));
+    void getBeerByIdTest() throws Exception {
+        mockMvc.perform(get("/api/v1/beer/" +UUID.randomUUID())
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
     }
 }
