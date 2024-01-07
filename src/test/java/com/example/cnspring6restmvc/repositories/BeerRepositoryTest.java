a5f70990220ece7832e7def91a7ae4a2634e3cc4
Cesar Noriega <c79.noriega@gmail.com>
2024-01-07 16:44:26 -0600

    Added JPA test splices.

---
+++ src/test/java/com/example/cnspring6restmvc/repositories/BeerRepositoryTest.java
@@ -0,0 +1,26 @@
+package com.example.cnspring6restmvc.repositories;
+
+import com.example.cnspring6restmvc.entities.Beer;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DataJpaTest
+class BeerRepositoryTest {
+    @Autowired
+    BeerRepository beerRepository;
+
+    @Test
+    void saveBeerWithRepoTest(){
+        Beer savedBeer = beerRepository.save(Beer.builder()
+                .beerName("ComsBeer")
+                .build());
+        assertThat(savedBeer).isNotNull();
+        assertThat(savedBeer.getId()).isNotNull();
+
+;   }
+    
+
+}
---
+++ src/test/java/com/example/cnspring6restmvc/repositories/CustomerRepositoryTest.java
@@ -0,0 +1,28 @@
+package com.example.cnspring6restmvc.repositories;
+
+import com.example.cnspring6restmvc.entities.Customer;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.test.autoconfigure.orm.jpa.AutoConfigureDataJpa;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DataJpaTest
+@EntityScan(basePackages = "com.example.cnspring6restmvc.entities")
+@AutoConfigureDataJpa
+class CustomerRepositoryTest {
+    
+    @Autowired
+    CustomerRepository customerRepository;
+
+    @Test
+    void testSaveCustomer() {
+        Customer customer = customerRepository.save(Customer.builder()
+                        .customerName("Cesar")
+                .build());
+
+        assertThat(customer.getId()).isNotNull();
+    }
+}
