595b00d5a3c45e5e53fd7dc5e5808e42a9c59723
Cesar Noriega <c79.noriega@gmail.com>
2024-01-15 15:14:41 -0600

    Added JPA Update Beer by Id Operation.

--- src/test/java/com/example/cnspring6restmvc/controller/BeerControllerIT.java
+++ src/test/java/com/example/cnspring6restmvc/controller/BeerControllerIT.java
@@ -2,6 +2,7 @@ package com.example.cnspring6restmvc.controller;
 
 import com.example.cnspring6restmvc.entities.Beer;
 import com.example.cnspring6restmvc.exception.NotFoundException;
+import com.example.cnspring6restmvc.mappers.BeerMapper;
 import com.example.cnspring6restmvc.model.BeerDTO;
 import com.example.cnspring6restmvc.repositories.BeerRepository;
 import jakarta.transaction.Transactional;
@@ -9,11 +10,15 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.annotation.Rollback;
 
 import java.util.List;
 import java.util.UUID;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 @SpringBootTest
 class BeerControllerIT {
@@ -24,12 +29,14 @@ class BeerControllerIT {
     @Autowired
     BeerRepository beerRepository;
 
+    @Autowired
+    BeerMapper beerMapper;
 
     @Test
     void testGetById(){
         Beer beer = beerRepository.findAll().get(0);
         BeerDTO beerDTO = beerController.getBeerById(beer.getId());
-        Assertions.assertThat(beerDTO).isNotNull();
+        assertThat(beerDTO).isNotNull();
     }
 
     @Test
@@ -41,16 +48,51 @@ class BeerControllerIT {
     @Test
     void testListBeer() {
         List<BeerDTO> dtos = beerController.listBeers();
-        Assertions.assertThat(dtos.size()).isEqualTo(3);
+        assertThat(dtos.size()).isEqualTo(3);
     }
 
     @Rollback
     @Transactional
     @Test
-    void estEmptyList(){
+    void testEmptyList(){
         beerRepository.deleteAll();
         List<BeerDTO> dtos = beerController.listBeers();
-        Assertions.assertThat(dtos.size()).isEqualTo(0);
+        assertThat(dtos.size()).isEqualTo(0);
 
+    }
+
+    @Rollback
+    @Transactional
+    @Test
+    void testSaveNewBeer() {
+        BeerDTO beerDTO = BeerDTO.builder()
+                .beerName("Polar")
+                .build();
+        ResponseEntity responseEntity = beerController.handlePost(beerDTO);
+
+        assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatusCode.valueOf(201));
+        assertThat(responseEntity.getHeaders().getLocation()).isNotNull();
+
+        String [] locationUUID = responseEntity.getHeaders().getLocation().getPath().split("/");
+        UUID savedUUID = UUID.fromString(locationUUID[4]);
+
+        Beer beer =  beerRepository.findById(savedUUID).get();
+        assertThat(beer).isNotNull();
+    }
+
+    @Test
+    void updateExistingBeer() {
+        Beer beer = beerRepository.findAll().get(0);
+        BeerDTO beerDTO = beerMapper.beerToBeerDto(beer);
+        beerDTO.setId(null);
+        beerDTO.setVersion(0L);
+        final String beerName = "Pilsen";
+        beerDTO.setBeerName(beerName);
+
+        ResponseEntity responseEntity = beerController.updateById(beer.getId(), beerDTO);
+        assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatusCode.valueOf(204));
+
+        Beer updatedBeer = beerRepository.findById(beer.getId()).get();
+        assertThat(beer).isNotNull();
     }
 }
