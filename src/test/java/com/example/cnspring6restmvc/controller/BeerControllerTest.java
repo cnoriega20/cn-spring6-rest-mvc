41aacfa2471feaa4d6f2b277347bbbe4b91b29a9
Cesar Noriega <c79.noriega@gmail.com>
2024-01-24 21:59:34 -0600

    Added mvcResult printed

--- src/test/java/com/example/cnspring6restmvc/controller/BeerControllerTest.java
+++ src/test/java/com/example/cnspring6restmvc/controller/BeerControllerTest.java
@@ -15,6 +15,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 
 import java.util.Optional;
 import java.util.UUID;
@@ -133,11 +134,13 @@ class BeerControllerTest {
 
         given(beerService.saveNewBeer(any(BeerDTO.class))).willReturn(beerServiceImpl.listBeers().get(1));
 
-        mockMvc.perform(post(BEER_PATH)
+        MvcResult mvcResult = mockMvc.perform(post(BEER_PATH)
                 .accept(MediaType.APPLICATION_JSON)
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(objectMapper.writeValueAsString(beerDTO)))
-        .andExpect(status().isBadRequest());
+        .andExpect(status().isBadRequest()).andReturn();
+
+        System.out.println("mvcResult: " + mvcResult.getResponse().getContentAsString());
 
     }
 }
