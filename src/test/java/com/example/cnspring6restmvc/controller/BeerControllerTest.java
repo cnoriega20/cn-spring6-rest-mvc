5ca58fc52028441b65b9b27fffd6346963d46837
Cesar Noriega <c79.noriega@gmail.com>
2024-01-25 00:15:44 -0600

    Added more validation to BeerDTO. Edited test.

--- src/test/java/com/example/cnspring6restmvc/controller/BeerControllerTest.java
+++ src/test/java/com/example/cnspring6restmvc/controller/BeerControllerTest.java
@@ -138,7 +138,9 @@ class BeerControllerTest {
                 .accept(MediaType.APPLICATION_JSON)
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(objectMapper.writeValueAsString(beerDTO)))
-        .andExpect(status().isBadRequest()).andReturn();
+        .andExpect(status().isBadRequest())
+        .andExpect(jsonPath("$.length()", is(2)))
+                .andReturn();
 
         System.out.println("mvcResult: " + mvcResult.getResponse().getContentAsString());
 
