bfc5e175705a7f7b425680d7456a6d2245f14650
Cesar Noriega <c79.noriega@gmail.com>
2024-02-22 01:12:44 -0600

    Added Beer CSV pojo.

---
+++ src/main/java/com/example/cnspring6restmvc/model/BeerCSVRecord.java
@@ -0,0 +1,24 @@
+package com.example.cnspring6restmvc.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class BeerCSVRecord {
+    private Integer row;
+    private Integer count;
+    private String abv;
+    private String ibu;
+    private Integer id;
+    private String beer;
+    private String style;
+    private Integer breweryId;
+    private Float ounces;
+    private String style2;
+    private String count_y;
+    private String city;
+    private String state;
+    private String label;
+
+}
