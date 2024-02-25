9962f0d42b8795875600aaea1de6f37c5b53cd20
Cesar Noriega <c79.noriega@gmail.com>
2024-02-25 16:38:39 -0600

    Added @Service.

--- src/test/java/com/example/cnspring6restmvc/services/BeerCsvServiceImpl.java
+++ src/test/java/com/example/cnspring6restmvc/services/BeerCsvServiceImpl.java
@@ -3,12 +3,14 @@ package com.example.cnspring6restmvc.services;
 import com.example.cnspring6restmvc.entities.Beer;
 import com.example.cnspring6restmvc.model.BeerCSVRecord;
 import com.opencsv.bean.CsvToBeanBuilder;
+import org.springframework.stereotype.Service;
 
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.util.List;
 
+@Service
 public class BeerCsvServiceImpl implements BeerCsvService {
     @Override
     public List<BeerCSVRecord> convertCSV(File csvFile) {
