4621323eacb8348ab319834333500f3464db90ed
Cesar Noriega <c79.noriega@gmail.com>
2024-03-19 00:24:57 -0500

    Added  One to Many Bidirectional.

---
+++ src/main/java/com/example/cnspring6restmvc/entities/BeerOrderLine.java
@@ -0,0 +1,48 @@
+package com.example.cnspring6restmvc.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.JdbcTypeCode;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.hibernate.type.SqlTypes;
+
+import java.sql.Timestamp;
+import java.util.UUID;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class BeerOrderLine {
+    @Id
+    @GeneratedValue(generator = "UUID")
+    @GenericGenerator(
+            name = "UUID",
+            strategy = "org.hibernate.id.UUIDGenerator"
+    )
+    @JdbcTypeCode(SqlTypes.CHAR)
+    @Column(length = 36, columnDefinition = "varchar(36)", updatable = false, nullable = false )
+    private UUID id;
+
+    @Version
+    private Long version;
+
+    @CreationTimestamp
+    @Column(updatable = false)
+    private Timestamp timestamp;
+
+    @UpdateTimestamp
+    private Timestamp lastModifiedDate;
+
+    public boolean isNew(){
+        return this.id == null;
+    }
+
+    private Integer orderQuantity = 0;
+    private Integer quantifyAllocated;
+
+}
--- src/main/java/com/example/cnspring6restmvc/entities/Customer.java
+++ src/main/java/com/example/cnspring6restmvc/entities/Customer.java
@@ -7,6 +7,7 @@ import org.hibernate.annotations.JdbcTypeCode;
 import org.hibernate.type.SqlTypes;
 
 import java.time.LocalDateTime;
+import java.util.Set;
 import java.util.UUID;
 
 @Getter
@@ -33,4 +34,7 @@ public class Customer {
     private Long version;
     private LocalDateTime createdDate;
     private LocalDateTime updateDate;
+
+    @OneToMany(mappedBy = "customer")
+    private Set<BeerOrder> beerOrders;
 }
